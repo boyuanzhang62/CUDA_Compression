543324bbe3f26a045140f23b8fd0990aeb6777db
Zhang_ Boyuan <boyuan.zhang1@sn14.mgmt.kamiak.wsu.edu>
2022-07-11 15:48:54 -0700

    add code to show the gpu time

--- culzss.c
+++ culzss.c
@@ -113,7 +113,7 @@ void *gpu_consumer (void *q)
 		
 		gettimeofday(&t2_end,0);
 		time_d = (t2_end.tv_sec-t2_start.tv_sec) + (t2_end.tv_usec - t2_start.tv_usec)/1000000.0;
-		//printf("GPU kernel took:\t%f \t", time_d);
+		printf("GPU kernel took:\t%f \t", time_d);
 				
 		pthread_mutex_lock (fifo->mut);
 		fifo->ledger[fifo->headGC]++;
@@ -127,7 +127,7 @@ void *gpu_consumer (void *q)
 		
 		gettimeofday(&t1_end,0);
 		alltime = (t1_end.tv_sec-t1_start.tv_sec) + (t1_end.tv_usec - t1_start.tv_usec)/1000000.0;
-		//printf("GPU whole took:\t%f \n", alltime);
+		printf("GPU whole took:\t%f \n", alltime);
 	}
 	
 	deleteGPUmem(fifo->in_d);
